4a441864b90227aa7713ed043ca8879450fdac5c
CBGhaha <875675253@qq.com>
2021-12-07 10:39:05 +0800

    fix

--- packages/baas-base-class/src/index.js
+++ packages/baas-base-class/src/index.js
@@ -1,5 +1,10 @@
-import createVueBaasComponent from './vue';
 
+import Vue from 'baas-vue-runtime';
+import vueWarp from './vue/index';
+
+function createVueBaasComponent(component) {
+  return vueWarp(Vue, component);
+}
 export {
   createVueBaasComponent
 };
--- packages/baas-base-class/src/vue.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'baas-vue-runtime';
-import wrap from '@vue/web-component-wrapper';
-export default function createVueBaasComponent(component) {
-  return wrap(Vue, component);
-}
